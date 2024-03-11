bc955bff22ed26e340df6a10644b8e22b208802f
HenriqueMartello <henrique.martello003@gmail.com>
2024-03-11 16:02:05 -0300

    Teste de alteracao producao

--- client/App.js
+++ client/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, World!</Text>
+      <Text>Finalmente hospedado agora!</Text>
       <StatusBar style="auto" />
     </View>
   );
