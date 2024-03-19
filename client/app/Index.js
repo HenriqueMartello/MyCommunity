b323a0fcb317f2bf04af843d6690fac45e59a7fc
HenriqueMartello <henrique.martello003@gmail.com>
2024-03-18 23:15:37 -0300

    Correcao rota index.js

--- client/app/Index.js
+++ client/app/Index.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router'
 
-export default function App() {
+export default function Page() {
   return (
     <View style={styles.container}>
         <Text>Tela: Index.js</Text>
