b7345a302329db0497621a13ef6f47bd8efa9503
Manoela Pereira <manoela.pereira@develcode.com.br>
2024-04-18 20:34:21 -0300

    feat: commit Fix Build Aplicativo/Rotas

--- client/src/app/_layout.jsx
+++ client/src/app/_layout.jsx
@@ -2,15 +2,23 @@ import { Stack } from "expo-router";
 
 // Função responsável pelo roteamento das telas, bem como nome de exibição no container no cabeçalho.
 export default function Layout() {
-    return (
-      <Stack>
-          <Stack.Screen name="index" options={{ title: "Home" }} />
-          <Stack.Screen name="AlterarUsuario" options={{ title: "Alteração de Cadastro de Usuários" }} />
-          <Stack.Screen name="ConsultaUsuarios" options={{ title: "Consulta de Usuários" }} />
-          <Stack.Screen name="RegisterPage" options={{ title: "Cadastro de Usuário" }} />
-          <Stack.Screen name="RegisterPage" options={{ title: "Cadastro de Usuário" }} />
-          <Stack.Screen name="System" options={{ title: "Sistema" }} />
-          <Stack.Screen name="Development" options={{ title: "Development" }} />
-      </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen
+        name="AlterarUsuario"
+        options={{ title: "Alteração de Cadastro de Usuários" }}
+      />
+      <Stack.Screen
+        name="ConsultaUsuarios"
+        options={{ title: "Consulta de Usuários" }}
+      />
+      <Stack.Screen
+        name="RegisterPage"
+        options={{ title: "Cadastro de Usuário" }}
+      />
+      <Stack.Screen name="System" options={{ title: "Sistema" }} />
+      <Stack.Screen name="Development" options={{ title: "Development" }} />
+    </Stack>
+  );
 }
